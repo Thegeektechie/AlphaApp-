14b37f1bb4d441c46ce0847fd822ce490c12442c
The Geek Techie <94162832+Thegeektechie@users.noreply.github.com>
2022-06-16 07:49:16 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿
+
 using System;
 
 namespace AlphaApp
@@ -7,6 +7,9 @@ namespace AlphaApp
   {
     static void Main(string[] args)
     {
+      
+      
+      
 
 Console.WriteLine("                            ----------------------Question 1--------------------");
 
@@ -26,13 +29,13 @@ int inputTwoLength = inputTwo.Length;
 
 Console.WriteLine("Your First Input has " + inputOneLength + " letters, While your Second input has " + inputTwoLength + " letters");
 
-int TotalLength = inputOneLength + inputTwoLength;
+int OverAllLength = inputOneLength + inputTwoLength;
 
-Console.WriteLine("The addition of your First and Second Input is " + TotalLength);
+Console.WriteLine("The addition of your First and Second Input is " + OverAllLength);
 
-  int True	= TotalLength * 3;
-  int False = TotalLength + 20;
-  Console.WriteLine((TotalLength > 20) ? "The answer is true, so therefore the length of the 2 input multiply 3 is " + True: "The answer is False, so therefore the length of the 2 input + 20 is " + False );
+  int Multiple	= OverAllLength * 3;
+  int Addition = OverAllLength + 20;
+  Console.WriteLine((OverAllLength > 20) ? "The resultant value is more than 20, so therefore the length of the 2 input multiply by 3 is " + Multiple: "The resultant value is more than 20, so therefore the length of the 2 input + 20 is " + Addition);
 
 
 Console.WriteLine("  ");
@@ -40,32 +43,53 @@ Console.WriteLine("  ");
 
 
 
+      
+      
+      
+      
+      
 
 Console.WriteLine("                            ----------------------Question 2--------------------");
  Console.WriteLine("Write a program to calculate the profit between the cost price of a product of $200.876 whose selling price is $255.425. The result should be in two decimal places.");
  Console.WriteLine ("Solution");
   Console.WriteLine("Profit = Selling Price - Cost Price.");
-  float SellingPrice = 255.425F;
-  float CostPrice = 200.876F;
+  double SellingPrice = 255.425F;
+  double CostPrice = 200.876F;
   Console.WriteLine ("Profit = 255.425 - 200.876");
-  float profit = (float)(SellingPrice - CostPrice);
+  double profit = (double)(SellingPrice - CostPrice);
   Console.WriteLine("Profit = $" + Math.Round(profit, 2));
   
 
 
+      
+      
+      
+      
+      
 
 
 Console.WriteLine("                            ----------------------Question 3--------------------");
 Console.WriteLine("  Get the index of the third character of any given string. 	- Add the value of the index to the length of the whole string.	- Convert the result of the above to a character " );
 Console.WriteLine("Solution");
+Console.WriteLine("  ");
+Console.WriteLine("Enter any word of your choice");
+string str1 = Console.ReadLine();
+int word = (int)str1[2];
+int str1length = str1.Length;
+int value = word + str1length;
+char character = (char)value;
+Console.WriteLine("The word you enter is " + str1);
+Console.WriteLine(str1 + " has " + str1length + " characters");
+Console.WriteLine("The third character of your input is " + word);
+Console.WriteLine("The addition of the value of the third index and the whole string is " + value);
+Console.WriteLine("The converson of the result to a character is letter " + character);
+  
 
 
 
 
 
-
-
-Console.WriteLine("                            ----------------------Question 4--------------------");
+Console.WriteLine("                            ----------- ----------Question 4--------------------");
 
   Console.WriteLine(" Write a program that reads two numbers from the console and prints the greater of them. Solve the problem without using conditional or the tenary operator statements.");
   Console.WriteLine("Solution");
@@ -87,6 +111,10 @@ Console.WriteLine("The larger input is " + Math.Max(FirstInput, SecondInput) );
 Console.WriteLine("  ");
 Console.WriteLine("  ");
 
+      
+      
+      
+      
 
 
 Console.WriteLine("                            ----------------------Question 5--------------------");
@@ -95,6 +123,7 @@ Console.WriteLine("Solution");
             
 
             
+     
             int num = 5;
             int sum = 0;
             bool parseTry = false; 
@@ -130,11 +159,15 @@ Console.WriteLine("Solution");
                 }
 
 
-
-
+            
+ 
+            
+                
 
 
     Console.WriteLine("  ");
+      
+      
 
             Console.WriteLine("                            ----------------------Question 6--------------------");
         Console.WriteLine("  ");
@@ -152,10 +185,31 @@ Console.WriteLine(".... Recorded");
 Console.WriteLine("Your First Input is " + Input1 + ", Your second input is " + Input2);
 Console.Write(Input1 + " + " + Input2 + " -  5 = ");
 int result = Input1 + Input2 - 5;
+int resultInput = Convert.ToInt32(Console.ReadLine());
+if (result == resultInput) 
+{
+  Console.WriteLine("You have entered the correct answer");
+}
+else{
+  Console.WriteLine("You have entered the wrong answer, The correct answer is " + result + "Relax and calculate well next time");
+}
 
 Console.Write(result);
+      
+      
+      
+      
 Console.WriteLine("  ");
 Console.WriteLine("  ");
+      
+      
+      
+      
+      
+      
+      
+      
+      
             Console.WriteLine("                            ----------------------Question 7--------------------");
   Console.WriteLine("  ");
             Console.WriteLine("  Write a program that asks for a digit (0-9), and depending on the input, shows the digit as a word (in English). Use a switch statement.");
